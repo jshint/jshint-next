e0bfc1edbc76a4225f8a68095f0a744325735ef4
Anton Kovalyov <anton@kovalyov.net>
2012-06-17 16:47:43 -0700

    New Linter class for more flexible usage.
    We need to go away from old JSHINT() function. This patch introduces a
    new class Linter that is used by old JSHINT() function.

--- src/jshint.js
+++ src/jshint.js
@@ -7,49 +7,56 @@ var reason    = require("./reason.js");
 var asherah   = require("./asherah.js");
 var constants = require("./constants.js");
 
-var JSHINT = function (args) {
-	var report = new utils.Report(args.code);
-	var tree = parser.parse(args.code, {
-		range:    true, // Include range-based location data.
-		loc:      true, // Include column-based location data.
-		comment:  true, // Include a list of all found code comments.
-		tokens:   true, // Include a list of all found tokens.
-		tolerant: true  // Don't break on non-fatal errors.
-	});
+function Linter(code) {
+	this.code    = code;
+	this.globals = {};
+	this.config  = {};
+	this.tree    = {};
+	this.events  = {};
+	this.report  = new utils.Report(code);
 
 	// Pre-populate globals array with reserved variables,
 	// standard ECMAScript globals and user-supplied globals.
-	var globals = _.extend(
-		{},
+
+	_.extend(
+		this.globals,
 		constants.reservedVars,
-		constants.ecmaIdentifiers,
-		args.predefined || {}
+		constants.ecmaIdentifiers
 	);
+}
 
-	// Check provided JavaScript code using three modules:
-	//
-	// * Reason checks for things that can break your application in
-	//   at least one browser and other potentially dangerous things
-	//   (loose comparison with null, confusing use of +, etc.)
-	//
-	// * Asherah enforces general JavaScript practices (semicolons, eval,
-	//   simpler array initialization, etc.)
-	//
-	// Each module returns its own report that then can be combined into
-	// the final report.
+Linter.prototype = {
+	setGlobals: function (globals) {
+		_.extend(this.globals, globals);
+	},
 
-	report.mixin(reason.parse({
-		tree:       tree,
-		code:       args.code,
-		predefined: globals
-	}));
+	parse: function () {
+		this.tree = parser.parse(this.code, {
+			range:    true, // Include range-based location data.
+			loc:      true, // Include column-based location data.
+			comment:  true, // Include a list of all found code comments.
+			tokens:   true, // Include a list of all found tokens.
+			tolerant: true  // Don't break on non-fatal errors.
+		});
 
-	// report.mixin(asherah.parse(tree, args.code));
+		this.report.mixin(reason.parse({
+			tree: this.tree,
+			code: this.code,
+			predefined: this.globals
+		}));
+	}
+};
+
+function JSHINT(args) {
+	var linter = new Linter(args.code);
+	linter.setGlobals(args.predefined || {});
+	linter.parse();
 
 	return {
-		tree:   tree,
-		report: report
+		tree:   linter.tree,
+		report: linter.report
 	};
 };
 
-exports.lint = JSHINT;
+exports.Linter = Linter;
+exports.lint   = JSHINT;
