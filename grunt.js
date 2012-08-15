4aee3b833204b095a49651d0433c61e371fa82ce
Wolfgang Kluge <wolfgang@klugesoftware.de>
2012-08-15 12:57:32 -0700

    Add coverage task to Grunt file.
    This patch adds a new task--cover--that runs all unit tests to collect
    coverage data. Use `grunt cover` to see the report or `grunt cover
    --server` to run the coveraje web server.
    
    Closes GH-14.
    
    Squashed commit of the following:
    
    commit f007515334b251fcc2e40864d5a061d27fadfa13
    Author: Anton Kovalyov <anton@kovalyov.net>
    Date:   Wed Aug 15 12:55:54 2012 -0700
    
        Fix path
    
    commit 2056626daa331daa47dfee284736d7d361b1ae32
    Author: Wolfgang Kluge <wolfgang@klugesoftware.de>
    Date:   Tue Aug 14 22:14:55 2012 +0200
    
        Fix path to jshint.js
    
    commit 834225d30f2135bfa1fee51220ae174cc321c267
    Author: Wolfgang Kluge <wolfgang@klugesoftware.de>
    Date:   Sun Aug 12 12:22:38 2012 +0200
    
        Add coverage task to gruntfile
    
        The task runs all unit tests to collect coverage data. Use `grunt cover`
        to see the report or `grunt cover --server` to run the coveraje web server.

--- grunt.js
+++ grunt.js
@@ -30,4 +30,60 @@ module.exports = function (grunt) {
 		grunt.file.mkdir("./dist");
 		grunt.file.write("./dist/jshint.js", bundle.bundle());
 	});
+
+	grunt.registerTask("cover", "Shows the test coverage report", function () {
+		var coveraje, done, runHelper, countdown;
+		var fileCount = 0;
+		var tests = {};
+		var useServer = !!grunt.option("server");
+
+		try {
+			coveraje = require("coveraje");
+		} catch (ex) {
+			grunt.log.error('coveraje not installed. '.red +
+				'Use "' + 'npm install coveraje'.bold + '"');
+			return false;
+		}
+
+		done = this.async();
+		runHelper = coveraje.runHelper;
+
+		grunt.file.expandFiles(grunt.config("test.all")).forEach(function (f) {
+			fileCount++;
+			tests[f] = function (context, instance) {
+				return runHelper("nodeunit", { reporterName: "minimal" })
+					.run(f)
+					.onComplete(function () {
+						if (!useServer)
+							countdown.one();
+					})
+				;
+			};
+		});
+
+		if (!useServer) {
+			countdown = runHelper.createCountdown(
+				runHelper
+					.createEmitter(function () {})
+					.onComplete(function () {
+						setTimeout(done, 200);
+					})
+					.onError(function () {
+						done(false, "timeout");
+					}),
+				fileCount,
+				5000
+			);
+		}
+
+		var c = coveraje.cover(
+			"var jshint = require(require('path').join('" + __dirname + "', 'src', 'jshint.js'));",
+			tests,
+			{
+				useServer: useServer,
+				globals: "node",
+				resolveRequires: ["*"]
+			}
+		);
+	});
 };
